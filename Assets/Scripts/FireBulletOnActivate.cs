b8a7a5f7d07a99f78be950c73fe5d2ac56a6dcbd
Albert Bermudez <albertbermudez31@gmail.com>
2023-05-05 20:06:47 -0500

    actualizacion

--- Assets/Scripts/FireBulletOnActivate.cs
+++ Assets/Scripts/FireBulletOnActivate.cs
@@ -6,6 +6,7 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class FireBulletOnActivate : MonoBehaviour
 
 {
+    public GameObject weapon;
     public GameObject bullet;
     public Transform spawnPoint;
     public float fireSpeed = 20;
@@ -24,9 +25,14 @@ public class FireBulletOnActivate : MonoBehaviour
     public void FireBullet(ActivateEventArgs arg)
 
     {
-       GameObject spawnedBullet =  Instantiate(bullet);
-       spawnedBullet.transform.position = spawnPoint.position;
-       spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
+        GameObject spawnedBullet = Instantiate(bullet);
+        spawnedBullet.transform.position = new Vector3(spawnPoint.position.x, spawnPoint.position.y, spawnPoint.position.z);
+        Vector3 bulletRotation = new Vector3(spawnPoint.rotation.eulerAngles.x + weapon.transform.rotation.eulerAngles.x, spawnPoint.eulerAngles.y + weapon.transform.rotation.eulerAngles.y, spawnPoint.eulerAngles.z + weapon.transform.rotation.eulerAngles.z);
+        spawnedBullet.transform.rotation = Quaternion.Euler(bulletRotation);
+         Quaternion rotation = Quaternion.Euler(0, 30, 0);
+        
+
+        spawnedBullet.GetComponent<Rigidbody>().velocity = spawnPoint.forward * fireSpeed;
        Destroy(spawnedBullet, 5);
     }
 }
