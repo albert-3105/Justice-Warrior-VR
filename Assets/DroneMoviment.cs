bd392814aad15bd94ff199d486bf98c8a4e6367c
albertbermudez <albertbermudez31@gmail.com>
2023-05-28 20:16:13 -0500

    aq

--- Assets/DroneMoviment.cs
+++ Assets/DroneMoviment.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class DroneMoviment : MonoBehaviour
 {
-  private Rigidbody droneRigidbody;
-  private Vector3 movementInput;
-  private float speed = 25f;
+    private Rigidbody droneRigidbody;
+    private Vector3 movementInput;
+    private float speed = 25f;
+    public GameObject Bullet;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,10 +16,26 @@ public class DroneMoviment : MonoBehaviour
     }
 
     // Update is called once per frame
-      void FixedUpdate()
+    void FixedUpdate()
     {
         // Aplicar la fuerza para el movimiento del dron
         Vector3 movement = transform.TransformDirection(movementInput) * speed;
         droneRigidbody.AddForce(movement);
+    }
+
+  
+
+    // Detectar colisiones
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Hola");
+        if (collision.gameObject.CompareTag("Bullet"))
+        {
+            Debug.Log("HolaMundo");
+            // Colisión con una bala, destruir el dron
+            Destroy(gameObject);
+            
+        
+        }
     }
 }
