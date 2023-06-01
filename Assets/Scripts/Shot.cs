ad9670d6a94d2dcc709aa0a86599fc009b7e44d5
albertbermudez <albertbermudez31@gmail.com>
2023-05-31 22:33:41 -0500

    Actualizacion
    -Se actualizo script para recargar arma
    -Se actualizo canvas para contador de armas
    -Se actualizop canvas contador de score.

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -2,50 +2,85 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using UnityEngine.UI;
+using TMPro;
 
 public class Shot : MonoBehaviour
 {
+    public int cantBalas; // Variable cantidad de balas
+    public int cantTotalBalas; // Variable cantidad de balas totales
+
+    public TextMeshProUGUI cantBalasTMP;
+    public TextMeshProUGUI cantTotalBalasTMP;
+
     public GameController gameController;
     public GameObject weapon;
     public GameObject bullet;
     public Transform spawnPoint;
     public float fireSpeed = 20;
-    public float cooldownTime = 0.25f; 
+    public float cooldownTime = 0.25f;
 
-    private bool canShoot = true; 
-    
+    private bool canShoot = true;
+
     void Start()
     {
         XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
         grabbable.activated.AddListener(FireBullet);
     }
 
-    void OnCollisionEnter (Collision obj)
-    {
-        gameController.TargetHit(obj.gameObject);
-    }
-
     public void FireBullet(ActivateEventArgs arg)
     {
         if (canShoot)
         {
-            GameObject spawnedBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
+            if (cantBalas > 0)
+            {
+                GameObject spawnedBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
 
-            Rigidbody bulletRigidbody = spawnedBullet.GetComponent<Rigidbody>();
-            bulletRigidbody.velocity = spawnPoint.forward * fireSpeed;
+                Rigidbody bulletRigidbody = spawnedBullet.GetComponent<Rigidbody>();
+                bulletRigidbody.velocity = spawnPoint.forward * fireSpeed;
 
-            spawnedBullet.transform.Rotate(new Vector3(-90f, 0f, 0f));
+                spawnedBullet.transform.Rotate(new Vector3(-90f, 0f, 0f));
 
-            Destroy(spawnedBullet, 5);
+                Destroy(spawnedBullet, 5);
 
-            canShoot = false; 
-            StartCoroutine(StartCooldown());
+                cantBalas--;
+                UpdateBulletCountText();
+
+                canShoot = false;
+                StartCoroutine(StartCooldown());
+            }
+            else
+            {
+                Debug.Log("¡No quedan balas! Recarga el arma.");
+            }
         }
     }
 
     IEnumerator StartCooldown()
     {
         yield return new WaitForSeconds(cooldownTime);
-        canShoot = true; 
+        canShoot = true;
+    }
+
+    public void RecargarArma()
+    {
+        if (cantBalas < cantTotalBalas)
+        {
+            int balasRestantes = cantTotalBalas - cantBalas;
+            cantBalas += balasRestantes;
+            UpdateBulletCountText();
+
+            Debug.Log("¡Arma recargada! Se agregaron " + balasRestantes + " balas.");
+        }
+        else
+        {
+            Debug.Log("No necesitas recargar el arma en este momento.");
+        }
+    }
+
+    private void UpdateBulletCountText()
+    {
+         cantBalasTMP.text = cantBalas.ToString();
+        cantTotalBalasTMP.text = cantTotalBalas.ToString();
     }
 }
